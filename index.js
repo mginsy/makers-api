9a406eb49b2bebc769046b94dd4f54e09ee4adf8
unknown <mnginsbe@usc.edu>
2021-04-21 18:39:04 -0700

    stuff

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.port  || 3000;
+
+app.get("/", (req,res) => {
+    console.log("In / route!");
+    res.send("Hello world");
+});
+
+app.listen(port, () => {
+    console.log("Example app listening.");
+});
